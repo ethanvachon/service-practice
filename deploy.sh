61e66670baaeeae29a12d3ab4097430f71b3ce80
ethanvachon <ethanhvachon@gmail.com>
2021-03-10 13:06:01 -0700

    finished deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 dotnet publish -c Release
-docker build -t docker-demo ./bin/Release/net5.0/publish
-docker tag docker-demo registry.heroku.com/winter20-docker-demo/web
-docker push registry.heroku.com/winter20-docker-demo/web
-heroku container:release web -a winter20-docker-demo
+docker build -t service-practice ./bin/Release/net5.0/publish
+docker tag service-practice registry.heroku.com/bloggr-ethanvach/web
+docker push registry.heroku.com/bloggr-ethanvach/web
+heroku container:release web -a bloggr-ethanvach
 echo press any key
 read end
